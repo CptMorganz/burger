eb6ebd845fdc9ed8f4d326a82ba61f47c1203f77
Jared <jmorgan4257@gmail.com>
2017-09-28 20:31:01 -0500

    seems to work but can't clear the DB

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Need to add some objects into the DB here --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Angus and Avacodo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Banzai Buger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mac and Cheeseburger', false);
--- server.js
+++ server.js
@@ -6,3 +6,27 @@ var methodOverride = require('method-override');
 var port = process.env.PORT || 3000;
 
 var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
